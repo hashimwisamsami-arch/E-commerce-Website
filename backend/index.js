73e33e2052d33b0e756163dbbfe89ec1a921e35b
Hashim <hashimwisamsami@email.com>
2026-05-10 16:04:44 +0300

    made userController.js and asyncHandler.js userModel.js userRouter createTooken.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import userRoutes from "./routes/userRoutes.js";
 //utils
 import connectDB from "../backend/config/db.js";
 
@@ -15,7 +15,5 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+app.use("/api/users", userRoutes);
 app.listen(port, () => console.log(`server running on port: ${port}`));
