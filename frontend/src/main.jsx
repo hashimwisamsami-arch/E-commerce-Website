4b5d0051c8b35b1e98872069129e432d66b125e9
Hashim <hashimwisamsami@email.com>
2026-05-14 18:18:07 +0300

    add UserList.jsx and use flowbite in index.css and add message component and add profile

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,16 +5,30 @@ import { Route, RouterProvider, createRoutesFromElements } from "react-router";
 import { createBrowserRouter } from "react-router";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
+//Private Route
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
 //Auth
 import Login from "./pages/Auth/Login.jsx";
 import Registetr from "./pages/Auth/Registetr.jsx";
+import Profile from "./pages/User/Profile.jsx";
+import AdminRoute from "./pages/Admin/AdminRoute.jsx";
+import UserList from "./pages/Admin/UserList.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route path="/login" element={<Login />}></Route>
       <Route path="/register" element={<Registetr />}></Route>
+
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/profile" element={<Profile />} />
+      </Route>
+
+      {/*Admin Routes*/}
+      <Route path="/admin" element={<AdminRoute />}>
+        <Route path="userlist" element={<UserList />} />
+      </Route>
     </Route>,
   ),
 );
