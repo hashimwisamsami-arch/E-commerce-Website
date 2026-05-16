38d96d617b3618a0d343948356cca0e5a58296b5
Hashim <hashimwisamsami@email.com>
2026-05-16 15:34:40 +0300

    add product model

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -29,6 +29,7 @@ const productSchema = mongoose.Schema(
     price: { type: Number, required: true, default: 0 },
     countInStock: { type: Number, required: true, default: 0 },
   },
+
   { timestamps: true },
 );
 const Product = mongoose.model("Product", productSchema);
