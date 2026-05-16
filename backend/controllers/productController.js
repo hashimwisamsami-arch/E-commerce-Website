0d51f9367ce3eaa9b1804e1c19776fa548ec8c90
Hashim <hashimwisamsami@email.com>
2026-05-16 15:59:44 +0300

    add create Prodacut

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,6 +2,31 @@ import asyncHandler from "../middlewares/asyncHandler.js";
 import Product from "../models/productModel.js";
 
 const addProduct = asyncHandler(async (req, res) => {
-  res.send("hello");
+  try {
+    const { name, description, price, category, quantity, brand } = req.fields;
+
+    //Validation
+    switch (true) {
+      case !name:
+        return res.json({ error: "Name is requaired" });
+      case !description:
+        return res.json({ error: "description is requaired" });
+      case !price:
+        return res.json({ error: "price is requaired" });
+      case !category:
+        return res.json({ error: "category is requaired" });
+      case !quantity:
+        return res.json({ error: "quantity is requaired" });
+      case !brand:
+        return res.json({ error: "brand is requaired" });
+    }
+
+    const product = new Product({ ...req.fields });
+    await product.save();
+    res.json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json(error.message);
+  }
 });
 export { addProduct };
