9b790d18d4e39d5f46de1c2267d4e22b6cee8dd3
Hashim <hashimwisamsami@email.com>
2026-05-17 14:33:50 +0300

    add update and delete and fetch Products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -29,4 +29,70 @@ const addProduct = asyncHandler(async (req, res) => {
     res.status(400).json(error.message);
   }
 });
-export { addProduct };
+
+const updateProductDetails = asyncHandler(async (req, res) => {
+  try {
+    const { name, description, price, category, quantity, brand } = req.fields;
+    //Validation
+    switch (true) {
+      case !name:
+        return res.json({ error: "Name is requaired" });
+      case !description:
+        return res.json({ error: "description is requaired" });
+      case !price:
+        return res.json({ error: "price is requaired" });
+      case !category:
+        return res.json({ error: "category is requaired" });
+      case !quantity:
+        return res.json({ error: "quantity is requaired" });
+      case !brand:
+        return res.json({ error: "brand is requaired" });
+    }
+
+    const product = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        ...req.fields,
+      },
+      { new: true },
+    );
+    await product.save();
+    res.json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json(error.message);
+  }
+});
+
+const removeProduct = asyncHandler(async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    res.json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
+  }
+});
+
+const fetchPoduct = asyncHandler(async (req, res) => {
+  try {
+    const pageSize = 6;
+    const keyword = req.query.keyword
+      ? { name: { $regex: req.query.keyword, $options: "i" } }
+      : {};
+
+    const count = await Product.countDocuments({ ...keyword });
+    const products = await Product.find({ ...keyword }).limit(pageSize);
+    res.json({
+      products,
+      page: 1,
+      pages: Math.ceil(count / pageSize),
+      hasMore: false,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
+  }
+});
+
+export { addProduct, updateProductDetails, removeProduct, fetchPoduct };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,9 +3,19 @@ import formidable from "express-formidable";
 import { authorizeAdmin, authenticate } from "../middlewares/authMiddleware.js";
 import checkId from "../middlewares/checkId.js";
 //controllers
-import { addProduct } from "../controllers/productController.js";
+import {
+  addProduct,
+  updateProductDetails,
+  removeProduct,
+  fetchPoduct,
+} from "../controllers/productController.js";
 
 const router = express.Router();
 router.route("/").post(authenticate, authorizeAdmin, formidable(), addProduct);
+router
+  .route("/:id")
+  .put(authenticate, authorizeAdmin, formidable(), updateProductDetails);
+router.route("/:id").delete(authenticate, authorizeAdmin, removeProduct);
+router.route("/").get(fetchPoduct);
 
 export default router;
